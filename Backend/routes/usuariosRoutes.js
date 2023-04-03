d7074bc1805efc605693ba55e2230caa0b6aaa29
EnzoCam7 <camerac09@gmail.com>
2023-04-03 15:27:17 -0300

    CheckAuth y Perfil

--- Backend/routes/usuariosRoutes.js
+++ Backend/routes/usuariosRoutes.js
@@ -1,5 +1,6 @@
 import express from "express";
-import { registrar, autenticar, confirmar, olvidePassword, comprobarToken, nuevoPassword } from "../controllers/usuarioController.js";
+import { registrar, autenticar, confirmar, olvidePassword, comprobarToken, nuevoPassword, perfil } from "../controllers/usuarioController.js";
+import checkAuth from "../middleware/checkAuth.js";
 
 const usuariosRouter = express.Router()
 
@@ -15,5 +16,7 @@ usuariosRouter.post('/olvide-password', olvidePassword)
 usuariosRouter.get('/olvide-password/:token', comprobarToken)
 
 usuariosRouter.post('/olvide-password/:token', nuevoPassword)
+
+usuariosRouter.get('/perfil', checkAuth, perfil)
 
 export default usuariosRouter
