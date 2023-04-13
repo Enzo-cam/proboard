da9122c1c73bf78f9f0f86caf818c5303c29e5dd
EnzoCam7 <camerac09@gmail.com>
2023-04-12 22:31:57 -0300

    Controller for projects

---
+++ Backend/controllers/proyectoController.js
@@ -0,0 +1,45 @@
+
+
+const getProjects = async (req, res) =>{
+
+}
+
+const getProject = async (req, res) =>{
+
+}
+
+const newProject = async (req, res) =>{
+
+}
+
+const editProject = async (req, res) =>{
+
+}
+
+const deleteProject = async (req, res) =>{
+
+}
+
+const addColaborator = async (req, res) =>{
+
+}
+
+const deleteColaborator = async (req, res) =>{
+
+}
+
+const obtainTasks = async (req, res) => {
+
+}
+
+export {
+    getProjects,
+    getProject,
+    newProject,
+    editProject,
+    deleteProject,
+    addColaborator,
+    deleteColaborator,
+    obtainTasks
+}
+
