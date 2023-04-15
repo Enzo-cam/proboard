a453c22ad1175f2fcd1da05db3c7f24e5b7f31d3
EnzoCam7 <camerac09@gmail.com>
2023-04-15 19:21:00 -0300

    Controller to get, edit and delete task

--- Backend/controllers/tareaController.js
+++ Backend/controllers/tareaController.js
@@ -2,48 +2,58 @@ import Tarea from "../Models/Tarea.js"
 import Proyecto from "../Models/Proyecto.js"
 
 const getTask = async (req, res) =>{
-    // All tasks that match the creator and the usuario who request.
-    const tasks = await Tarea.find().where('creador').equals(req.usuario)
+    // We show the task referer to the id that we receive.
+    const {id} = req.params
+    const task = await Tarea.findById(id).populate("proyecto")
 
-    res.json(tasks)
+    if(!task){
+        const error = new Error('La tarea no existe..')
+        return res.status(404).json({ msg: error.message })
+    }else if(task.proyecto.creador.toString() !== req.usuario._id.toString()){
+        const error = new Error('Accion no válida.')
+        return res.status(403).json({ msg: error.message })
+    }
+
+    res.json(task)
 }
 
 const newTask = async (req, res) =>{
-    // New instance of project and asign the creator.
+    // We obtain the project based in the ID that we pass in the req.body
     const {proyecto} = req.body
+    // Then we check if that project exist.
     const existeProy = await Proyecto.findById(proyecto)
 
     if(!existeProy){
         const error = new Error('El proyecto no existe..')
         return res.status(404).json({ msg: error.message })
     }else if(existeProy.creador.toString() !== req.usuario._id.toString()){
-        return res.status(404).json({ msg: "Acción no válida." })
+        return res.status(403).json({ msg: "Acción no válida." })
     }
 
+    // If the project exist, we saved the task relationated with that project.
     try {
         const savedTask = await Tarea.create(req.body)
         res.json(savedTask)
     } catch (error) {
         console.log(error)
     }
-
-
 }
-
 
 const updateTask = async (req, res) =>{
     const {id} = req.params
-    const task = await Tarea.findById(id)
+    const task = await Tarea.findById(id).populate("proyecto")
 
     if(!task){
         const error = new Error('Tarea no encontrada..')
         return res.status(404).json({ msg: error.message })
-    }else if(task.creador.toString() !== req.usuario._id.toString()){
+    }else if(task.proyecto.creador.toString() !== req.usuario._id.toString()){
         return res.status(404).json({ msg: "Acción no válida." })
     }
 
     task.nombre = req.body.nombre || task.nombre
     task.descripcion = req.body.descripcion || task.descripcion
+    task.prioridad = req.body.prioridad || task.prioridad
+    task.fechaEntrega = req.body.fechaEntrega || task.fechaEntrega
 
     try {
         const editedTask = await task.save()
@@ -51,16 +61,17 @@ const updateTask = async (req, res) =>{
     } catch (error) {
         console.log(error)
     }
+
 }
 
 const deleteTask = async (req, res) =>{
     const {id} = req.params
-    const task = await Tarea.findById(id)
+    const task = await Tarea.findById(id).populate("proyecto")
 
     if(!task){
         const error = new Error('Tarea no encontrada..')
         return res.status(404).json({ msg: error.message })
-    }else if(task.creador.toString() !== req.usuario._id.toString()){
+    }else if(task.proyecto.creador.toString() !== req.usuario._id.toString()){
         return res.status(404).json({ msg: "Acción no válida." })
     }
 
@@ -73,7 +84,7 @@ const deleteTask = async (req, res) =>{
 }
 
 const changeStateTask = async (req, res) =>{
-
+    
 }
 
 
