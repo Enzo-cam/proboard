46443496462055eb434d6f14cf1d4c86b563dc49
EnzoCam7 <camerac09@gmail.com>
2023-04-14 21:10:52 -0300

    Imported 'tareasRouter' and the endpoint

--- Backend/index.js
+++ Backend/index.js
@@ -3,16 +3,17 @@ import dotenv from "dotenv"
 import connectDB from "./config/db.js"
 import usuariosRouter from "./routes/usuariosRoutes.js"
 import proyectosRouter from "./routes/proyectoRoutes.js"
+import tareasRouter from "./routes/tareaRoutes.js"
 
 const app = express()
 app.use(express.json())
 dotenv.config()
 connectDB()
 
-
 // Routing
 app.use('/api/usuarios', usuariosRouter)
 app.use('/api/proyectos', proyectosRouter)
+app.use('/api/tareas', tareasRouter)
 
 
 app.listen(5000, () => {
