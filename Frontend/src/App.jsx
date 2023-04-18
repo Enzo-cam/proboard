d24ef3798dc11a9ff7d9cf5e99620c7e03fc72b6
EnzoCam7 <camerac09@gmail.com>
2023-04-18 18:01:58 -0300

    Added: routes and layouts.

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,25 @@
-import {BrowserRouter, Router, Route} from "react-router-dom"
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import AuthLayout from "./layout/AuthLayout"
+
+import Login from "./paginas/Login"
+import Registrar from "./paginas/Registrar"
+import NuevoPass from "./paginas/NuevoPass"
+import OlvidePass from "./paginas/OlvidePass"
+import ConfirmarCuenta from "./paginas/ConfirmarCuenta"
 
 function App() {
 
   return (
     <BrowserRouter>
-      <Router>
-        <Route>
-          
+      <Routes>
+        <Route path="/" element={<AuthLayout/>}>
+          <Route index element={<Login />} />
+          <Route path="registrar" element={<Registrar />} />
+          <Route path="olvide-password" element={<OlvidePass />} />
+          <Route path="olvide-password/:token" element={<NuevoPass />} />
+          <Route path="confirmar/:id" element={<ConfirmarCuenta />} />
         </Route>
-      </Router>
+      </Routes>
     </BrowserRouter>
   )
 }
