11c08ddcc7414c6c6b395ba31385609f7c0b3a45
EnzoCam7 <camerac09@gmail.com>
2023-04-19 20:23:48 -0300

    Added styles

--- Frontend/src/layout/AuthLayout.jsx
+++ Frontend/src/layout/AuthLayout.jsx
@@ -3,9 +3,12 @@ import { Outlet } from "react-router-dom"
 const AuthLayout = () => {
   return (
     <>
-        <div>AuthLayout</div>
-        {/* Renderiza todos los componentes que esten dentro de este ROUTE. */}
-        <Outlet />
+        <main className="container mx-auto mt-5 md:mt-20 md:flex md:justify-center">
+          <div className="md:w-2/3 lg:w-2/5 max-sm:my-10">
+            {/* Renderiza todos los componentes que esten dentro de este ROUTE. */}
+            <Outlet />
+          </div>
+        </main>
     </>
   )
 }
