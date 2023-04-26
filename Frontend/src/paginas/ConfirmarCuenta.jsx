2535bcb1d951f4e85ded41fd6e416fab9b5c9ddd
EnzoCam7 <camerac09@gmail.com>
2023-04-26 19:16:29 -0300

    ConfirmarCuenta component

--- Frontend/src/paginas/ConfirmarCuenta.jsx
+++ Frontend/src/paginas/ConfirmarCuenta.jsx
@@ -1,8 +1,14 @@
-
 const ConfirmarCuenta = () => {
   return (
-    <div>ConfirmarCuenta</div>
-  )
-}
+    <>
+        <h3 className="text-center mb-6 text-slate-800 font-bold text-3xl">
+          Confirme su cuenta para empezar a usar{" "}
+          <span className="text-amber-900">ProBoard</span>
+        </h3>
+        
+        
+    </>
+  );
+};
 
-export default ConfirmarCuenta
+export default ConfirmarCuenta;
