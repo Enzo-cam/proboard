405dc71a48dd982f59f307cc34137b7a6a14a6da
EnzoCam7 <camerac09@gmail.com>
2023-05-14 20:15:57 -0300

    Added CORS

--- Backend/index.js
+++ Backend/index.js
@@ -4,11 +4,27 @@ import connectDB from "./config/db.js"
 import usuariosRouter from "./routes/usuariosRoutes.js"
 import proyectosRouter from "./routes/proyectoRoutes.js"
 import tareasRouter from "./routes/tareaRoutes.js"
+import cors from 'cors'
 
 const app = express()
 app.use(express.json())
 dotenv.config()
 connectDB()
+
+// Habilitar CORS
+const whitelist = [process.env.FRONT_URL]
+
+const corsOptions = {
+    origin : function(origin, callback){
+        if(whitelist.includes(origin)){
+            callback(null, true)
+        }else{
+            callback(new Error('Error de CORS'))
+        }
+    }
+}
+
+app.use(cors(corsOptions))
 
 // Routing para cada endpoint
 app.use('/api/usuarios', usuariosRouter)
